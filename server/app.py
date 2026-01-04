1950c64e783382b2f2c17271e236e324577baec6
Hidaya Mohamed <hidayamohamed002@gmail.com>
2026-01-04 22:01:30 +0300

    Added configurations to app.py

---
+++ server/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.json.compact = False
+
+db.init_app(app)
+
+
+
+    
