9727cb1ccc3a09d968b53d69152891226418643b
Hidaya Mohamed <hidayamohamed002@gmail.com>
2026-01-04 23:11:34 +0300

    ADded a get_heroes in app.py

--- server/app.py
+++ server/app.py
@@ -1,18 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-
-db = SQLAlchemy()
-migrate = Migrate(app, db)
-
+from models import db, Hero, Power, HeroPower
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
+migrate = Migrate(app, db)
 db.init_app(app)
+
+
+@app.get("/heroes")
+def get_heroes():
+    heroes = Hero.query.all()
+    return jsonify([
+        hero.to_dict(only=("id", "name", "super_name"))
+        for hero in heroes
+    ])
+
+
+
+
+
+
+
 
 
 
